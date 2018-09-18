ddb184130fa9cf03d0ce9eb5f7c8dfc50af1cc56
chris574topher <chris574topher@gmail.com>
2018-09-18 11:13:01 -0400

    codeclimate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,12 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require 'simplecov'
-SimpleCov.start 'rails' do
-  add_filter '/bin/'
-  add_filter '/db/'
-  add_filter '/spec/' # for rspec
-  add_filter '/test/' # for minitest
-end
+SimpleCov.start 'rails'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
